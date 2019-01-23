62afd36503630d703a6c7b22d1c4f596d6eb53a8
danisi7f <46641012+danisi7f@users.noreply.github.com>
2019-01-23 14:34:31 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 <div class="footer" id="user">
     <div class="row">
         <div class="col-xs-12">
-            Copyright &COPY; <?= date('Y')?> Developer Kampung
+            Copyright &COPY; <?= date('Y')?> Developer Kampoeng  |
             <a href="#">Dokumentasi</a>
             <a href="#">Peta Situs</a>
             <a href="#">Support</a>
